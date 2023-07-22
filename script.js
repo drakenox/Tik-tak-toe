8a997bd9e04774decb28d0b2566db51a1e770669
Drake <saranwarlord@gmail.com>
2023-07-22 17:55:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cells = document.querySelectorAll(".cell");
+  const message = document.getElementById("message");
+  const restartButton = document.getElementById("restartButton");
+
+  // ... (Rest of the JavaScript code remains unchanged)
+});
